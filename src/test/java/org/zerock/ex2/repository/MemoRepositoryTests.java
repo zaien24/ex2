671aa2cd162905e6c541ed7d9815d1133da17111
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-28 08:39:56 +0900

    p57 테스트 코드를 통한 CRUD연습

---
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,19 @@
+package org.zerock.ex2.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass(){
+
+        System.out.println(memoRepository.getClass().getName());
+
+    }
+}
