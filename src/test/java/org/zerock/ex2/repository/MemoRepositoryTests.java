f8d85a496fab9a59f02decf60a76fb919032811c
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-10-31 23:47:25 +0900

    p62 수정 작업 테스트

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -71,5 +71,16 @@ public class MemoRepositoryTests {
         System.out.println(memo);
     }
 
+    @Test
+    public void testUpdate() {
+
+        Memo memo = Memo.builder().mno(100L).memoText("Update Text").build();
+
+        System.out.println(memoRepository.save(memo));
+
+    }
+
+
+
 }
 
